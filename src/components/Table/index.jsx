4618f65316634229135d4cc57d06ddeaa6315e49
Jeferson Soares <jpadilha53@gmail.com>
2024-07-19 14:08:37 -0300

    codigo refatorado para utilizando o usestate e criando um array de objetos chamado tabelaIMC

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,61 +1,90 @@
 import style from "./Table.module.css";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 const Table = ({ dados }) => {
-  const [tabela, setTabela] = useState();
-  const elementosLi = useRef(null);
+  // const elementosLi = useRef(null);
+  // const verificaTabelaIMC = () => {
+  //   //pega todos elementos li
+  //   const elementos = elementosLi.current.querySelectorAll("li");
 
-  const verificaTabelaIMC = () => {
-    //pega todos elementos li
-    const elementos = elementosLi.current.querySelectorAll("li");
+  //   elementos.forEach((element) => {
+  //     const id = parseInt(element.id);
+  //     const valorSelecionado =
+  //       (dados <= 18.5 && id === 1) ||
+  //       (dados > 18.5 && dados <= 24.9 && id === 2) ||
+  //       (dados > 24.9 && dados <= 29.9 && id === 3) ||
+  //       (dados > 29.9 && dados <= 34.9 && id === 4) ||
+  //       (dados > 34.9 && dados <= 39.9 && id === 5) ||
+  //       (dados > 39.9 && id === 6);
 
-    elementos.forEach((element) => {
-      const id = parseInt(element.id);
-      const valorSelecionado =
-        (dados <= 18.5 && id === 1) ||
-        (dados > 18.5 && dados <= 24.9 && id === 2) ||
-        (dados > 24.9 && dados <= 29.9 && id === 3) ||
-        (dados > 29.9 && dados <= 34.9 && id === 4) ||
-        (dados > 34.9 && dados <= 39.9 && id === 5) ||
-        (dados > 39.9 && id === 6);
+  //     if (dados && valorSelecionado) {
+  //       element.classList.add(style.tableItemActive);
+  //     } else {
+  //       element.classList.remove(style.tableItemActive);
+  //     }
+  //   });
+  // };
 
-      if (dados && valorSelecionado) {
-        element.classList.add(style.tableItemActive);
-      } else {
-        element.classList.remove(style.tableItemActive);
-      }
-    });
+  // useEffect(() => {
+  //   verificaTabelaIMC();
+  // }, [dados]);
+  const [itensAtivos, setItensAtivos] = useState([]);
 
-    setTabela(dados);
-  };
+  const tabelaIMC = [
+    { id: 1, faixa: "Abaixo de 18,5:", descricao: "Abaixo do peso" },
+    { id: 2, faixa: "18,6 a 24,9:", descricao: "Peso normal (ou eutrófico)" },
+    { id: 3, faixa: "25,0 a 29,9:", descricao: "Sobrepeso" },
+    { id: 4, faixa: "30,0 a 34,9:", descricao: "Obesidade grau I" },
+    { id: 5, faixa: "35,0 a 39,9:", descricao: "Obesidade grau II" },
+    {
+      id: 6,
+      faixa: "Acima de 40:",
+      descricao: "Obesidade grau III (obesidade mórbida)",
+    },
+  ];
 
   useEffect(() => {
-    verificaTabelaIMC();
+    const mostraItemAtivo = tabelaIMC
+      .filter((item) => {
+        switch (item.id) {
+          case 1:
+            return dados > 0 && dados <= 18.5;
+          case 2:
+            return dados > 18.5 && dados <= 24.9;
+          case 3:
+            return dados > 24.9 && dados <= 29.9;
+          case 4:
+            return dados > 29.9 && dados <= 34.9;
+          case 5:
+            return dados > 34.9 && dados <= 39.9;
+          case 6:
+            return dados > 39.9;
+          default:
+            return false;
+        }
+      })
+      .map((item) => item.id);
+
+    setItensAtivos(mostraItemAtivo);
   }, [dados]);
 
   return (
     <div className={style.containerTable}>
       <h2 className={style.imc}>Seu IMC: {dados}</h2>
-      <ul className={style.table} ref={elementosLi}>
+      <ul className={style.table}>
         <h2 className={style.tableName}>Tabela IMC</h2>
-        <li id="1" className={style.tableItem}>
-          <b>Abaixo de 18,5: </b>Abaixo do peso
-        </li>
-        <li id="2" className={style.tableItem}>
-          <b>18,6 a 24,9: </b>Peso normal (ou eutrófico)
-        </li>
-        <li id="3" className={style.tableItem}>
-          <b>25,0 a 29,9:</b> Sobrepeso
-        </li>
-        <li id="4" className={style.tableItem}>
-          <b>30,0 a 34,9: </b>Obesidade grau I
-        </li>
-        <li id="5" className={style.tableItem}>
-          <b>35,0 a 39,9:</b>Obesidade grau II
-        </li>
-        <li id="6" className={style.tableItem}>
-          <b>Acima de 40:</b> Obesidade grau III (obesidade mórbida)
-        </li>
+        {tabelaIMC.map((item) => (
+          <li
+            key={item.id}
+            id={item.id}
+            className={`${style.tableItem} ${
+              itensAtivos.includes(item.id) ? style.tableItemActive : ""
+            }`}
+          >
+            {" "}
+            <b>{item.faixa}</b> {item.descricao}
+          </li>
+        ))}
       </ul>
     </div>
   );
